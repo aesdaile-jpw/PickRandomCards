b9d1ad54593ace5c070b314092b711b538e26bcd
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-03-27 15:15:07 +1100

    p 140

---
+++ CardPicker.cs
@@ -0,0 +1,17 @@
+﻿namespace PickRandomCards;
+
+class CardPicker
+{
+    public static string[] PickSomeCards(int numberOfCards)
+    {
+        string[] pickedCards = new string[numberOfCards];
+
+        for (int i = 0;  i < numberOfCards; i++)
+        {
+            pickedCards[i] = RandomValue() + " of " + RandomSuit();
+        }
+        return pickedCards;
+    }
+
+
+}
